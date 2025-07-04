daf7a22a0e735a327f1097d858d193522ccfe1d8
yeji-star <sizuku134@gmail.com>
2025-07-04 09:54:41 +0900

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
     }
 }
